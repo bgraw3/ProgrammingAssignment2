e564d38acdb5e23a50f77a232aef1d3b24e1ab94
Bob Graw <bob.graw@CallTrackingMetrics.com>
2014-08-16 22:03:22 -0400

    caching inverse of a matrix with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculating the inverse of a matrix can be time consuming.
+## Use the following functions to create and calculate the inverse of a matrix.
 
-## Write a short comment describing this function
+# EXAMPLE usage
+# Note that calling cacheSolve the second time returns cached data
+#
+# m <- matrix(c(4,3,3,2), nrow = 2, ncol = 2, byrow=TRUE)
+# l <- makeCacheMatrix(m)
+# cacheSolve(l)
+#      [,1] [,2]
+# [1,]   -2    3
+# [2,]    3   -4
+#
+# cacheSolve(l)
+# getting cached data
+#      [,1] [,2]
+# [1,]   -2    3
+# [2,]    3   -4
 
-makeCacheMatrix <- function(x = matrix()) {
+## Create a special matrix that is a list of functions to...
+## - set the value the matrix
+## - get the value the matrix
+## - set the value of the inverse of the matrix
+## - get the value of the inverse of the matrix
 
+makeCacheMatrix <- function(m = matrix()) {
+
+  inverse <- NULL
+  set <- function(n) {
+    m <<- n
+    inverse <<- NULL
+  }
+  get <- function() m
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
+## Calculate the inverse of a special matrix.
+## If the inverse has already been found, return the cached version.
+## If the inverse has not been found, find the inverse, cache the inverse,
+## and return the inverse.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {  
+  inverse <- x$getinverse()
+  
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  
+  inverse
 }
+
+#
+# playing w/matrices
+#
+# m <- matrix(c(4,3,3,2), nrow = 2, ncol = 2, byrow=TRUE)
+#      [,1] [,2]
+# [1,]    4    3
+# [2,]    3    2
+#
+# solve(m)
+#      [,1] [,2]
+# [1,]   -2    3
+# [2,]    3   -4
